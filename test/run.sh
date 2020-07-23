cce91e3e1b3bf7624f9766f89d7d4cd9a7b58556
Thomas Duigou <thomas.duigou@inrae.fr>
2020-07-23 11:00:00 +0200

    remove test output folders before testing

--- test/run.sh
+++ test/run.sh
@@ -5,11 +5,12 @@ printf "\n"
 tput bold
 printf "Test normal case\n"
 tput sgr0
-
-python3 ../src/RP2paths.py all in/rp2_pathways.csv --outdir out_normal_case --timeout 5
+if [ -d "out_normal" ]; then rm -Rf out_normal; fi
+python3 ../src/RP2paths.py all in/rp2_pathways.csv --outdir out_normal --timeout 5
 printf "\n"
 
 tput bold
 printf "Test no scope matrix produced"
 tput sgr0
+if [ -d "out_no_scope" ]; then rm -Rf out_no_scope; fi
 python3 ../src/RP2paths.py all in/rp2_pathways_nomatrix.csv --outdir out_no_scope --timeout 5
