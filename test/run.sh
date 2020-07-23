fff2a1802ee3ede0b660757381c8870ae3c6808c
Thomas Duigou <thomas.duigou@inrae.fr>
2020-07-23 10:57:00 +0200

    distinct output folder each test

--- test/run.sh
+++ test/run.sh
@@ -5,10 +5,11 @@ printf "\n"
 tput bold
 printf "Test normal case\n"
 tput sgr0
-python3 ../src/RP2paths.py all in/rp2_pathways.csv --outdir out --timeout 5
+
+python3 ../src/RP2paths.py all in/rp2_pathways.csv --outdir out_normal_case --timeout 5
 printf "\n"
 
 tput bold
 printf "Test no scope matrix produced"
 tput sgr0
-python3 ../src/RP2paths.py all in/rp2_pathways_nomatrix.csv --outdir out --timeout 5
+python3 ../src/RP2paths.py all in/rp2_pathways_nomatrix.csv --outdir out_no_scope --timeout 5
